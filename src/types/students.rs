74b70f2b1d8acc85705883eb301dd82a66e3a945
Edelweiss <redwitblue@gmail.com>
2023-04-02 00:22:34 -0500

    Set `Age` as `pub`

--- src/types/students.rs
+++ src/types/students.rs
@@ -83,7 +83,7 @@ pub struct Student {
 ///
 /// The actual [`Option<u8>`] is wrapped under this struct to make it easier to display.
 #[derive(Debug)]
-pub struct Age(Option<u8>);
+pub struct Age(pub Option<u8>);
 
 impl std::fmt::Display for Age {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
