ea8aed58a616c8c6b715454abc9e0c0a7d8f0a15
Edelweiss <redwitblue@gmail.com>
2023-04-04 18:34:07 -0500

    Remove NotFound (#6)
    redundant

--- src/api/errors.rs
+++ src/api/errors.rs
@@ -5,11 +5,6 @@ use thiserror::Error;
 /// Contains a set of enum values for more context on what goes wrong.
 #[derive(Debug, Error)]
 pub enum BlueArchiveError {
-    #[error("the entity ({query}) was not found!")]
-    NotFound {
-        /// The name inserted by the user.
-        query: String,
-    },
     #[error("requesting data failed. ({0})")]
     Reqwest(#[from] reqwest::Error),
 }
