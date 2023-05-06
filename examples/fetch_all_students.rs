0cde4986a187c5c090e82ccce6bc9e334f03c57c
Edelweiss <redwitblue@gmail.com>
2023-05-06 00:16:19 -0500

    update version to 0.3.7

--- examples/fetch_all_students.rs
+++ examples/fetch_all_students.rs
@@ -3,7 +3,7 @@ use blue_archive::Language;
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     // Let us fetch some students. This time, we will use the Korean Language enum.
-    let students = blue_archive::fetch_all_students(&Language::Korean).await?;
+    let students = blue_archive::fetch_all_students(Language::Korean).await?;
     // Time to iterate over each student and print their displayed implementation!
     students.iter().for_each(|student| println!("{student}"));
     Ok(())
