dc9d3f486af944e0ae7e4d0c33b3e0c08fad2725
Edelweiss <redwitblue@gmail.com>
2024-02-29 02:07:03 -0600

    Update Guessing Game

--- examples/guessing_game.rs
+++ examples/guessing_game.rs
@@ -1,4 +1,4 @@
-use blue_archive::Language;
+use blue_archive::{types::Student, Language};
 use rand::Rng;
 
 fn read_line_to_buffer(buffer: &mut String) -> Result<usize, std::io::Error> {
@@ -10,19 +10,33 @@ fn read_line_to_buffer(buffer: &mut String) -> Result<usize, std::io::Error> {
 async fn main() -> anyhow::Result<()> {
     let mut input_buffer = String::new();
 
-    println!("Guessing Game");
+    println!("Guessing Game (it's really bad)");
     println!("---------------------------");
     println!("See if you can guess the characters based on certain properties.\n\n");
 
     let chosen = blue_archive::fetch_random_student(Language::English)
         .await?
         .unwrap();
-    let mut changed_name = &chosen.name.clone();
-    let tng = rand::thread_rng().gen_range(0..changed_name.len());
+    let mut changed_name = chosen.name.clone();
 
-    println!("Who is this character's name? [{chosen}]: ");
+    let index = rand::thread_rng().gen_range(0..chosen.name.len());
+    changed_name.remove(index);
+    changed_name.insert(index, '_');
+
+    println!("Who is this character's name? [{changed_name}]: ");
 
     read_line_to_buffer(&mut input_buffer)?;
+
+    println!("Your input: {}", input_buffer.trim());
+
+    if input_buffer.trim().to_lowercase() == chosen.name.to_lowercase() {
+        println!("Yay! You got it right!")
+    } else {
+        println!(
+            "Aw... you got it wrong... it was actually: {}.",
+            chosen.name
+        )
+    }
 
     Ok(())
 }
